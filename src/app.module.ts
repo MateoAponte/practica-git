2c262c3f6cbd0f14add1627ddc25f9782e4d3bb5
Paulina <fpaulina545@gmail.com>
2025-10-06 20:12:59 -0500

    feature: Se agrega el modulo notifiaciones con controller, service y module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
 import { UsersModule } from './users/users.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [ChatsModule, UsersModule],
+  imports: [ChatsModule, UsersModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
